944c094a04980c6e8e9c34fb2784abe4ae5761ef
SAMUEL NTI SARPONG <ce-sntisarpong8921@st.umat.edu.gh>
2025-09-17 22:00:12 +0000

    Try fixing this error: `Runtime Error: Error: Invalid supabaseUrl: Must

--- src/lib/supabase/client.ts
+++ src/lib/supabase/client.ts
@@ -3,7 +3,7 @@ import type { Database } from '@/lib/types';
 
 export function createClient() {
   return createBrowserClient<Database>(
-    process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+    "https://ggrjwnsdsulmmymrygvq.supabase.co",
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imdncmp3bnNkc3VsbW15bXJ5Z3ZxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTk1ODU2MjUsImV4cCI6MjAzNTE2MTYyNX0.CSoY0-hFwV2u2n_6n22vD5Lj_2i5fA_4R2Pb_8Fdgq8"
   );
 }
