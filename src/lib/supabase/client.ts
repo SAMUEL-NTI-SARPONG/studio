f36522f60a002860e3ee259519116b992e07035d
SAMUEL NTI SARPONG <ce-sntisarpong8921@st.umat.edu.gh>
2025-09-18 09:27:45 +0000

    project url: https://kztkzgflpbicrskjfvca.supabase.co anon api key: eyJh

--- src/lib/supabase/client.ts
+++ src/lib/supabase/client.ts
@@ -3,7 +3,7 @@ import type { Database } from '@/lib/types';
 
 export function createClient() {
   return createBrowserClient<Database>(
-    "YOUR_NEW_SUPABASE_URL",
-    "YOUR_NEW_SUPABASE_ANON_KEY"
+    'https://kztkzgflpbicrskjfvca.supabase.co',
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imt6dGt6Z2ZscGJpY3Jza2pmdmNhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTgxODU3NzksImV4cCI6MjA3Mzc2MTc3OX0.D7RZq8DczF4UiouZFp8lc-qaszQ7g75V1f9x6vbOj9g'
   );
 }
