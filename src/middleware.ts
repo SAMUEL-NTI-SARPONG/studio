37c368162617571ed2b1d402d4c34fd154f97160
SAMUEL NTI SARPONG <ce-sntisarpong8921@st.umat.edu.gh>
2025-09-18 06:46:59 +0000

    project url: https://eltuoucnxkyexffeaxdc.supabase.co project anon api k

--- src/middleware.ts
+++ src/middleware.ts
@@ -11,8 +11,8 @@ export async function middleware(request: NextRequest) {
   });
 
   const supabase = createServerClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL! || "https://eyelrvkcsbatuvxsnurf.supabase.co",
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY! || "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImV5ZWxydmtjc2JhdHV2eHNudXJmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTgxMzMxMzUsImV4cCI6MjA3MzcwOTEzNX0.1uHkXPKBgft2zRRw0On1cJniZTseIQjU-n08SBugKtM",
+    "https://eltuoucnxkyexffeaxdc.supabase.co",
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVsdHVvdWNueGt5ZXhmZmVheGRjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTgxNzA2NTMsImV4cCI6MjA3Mzc0NjY1M30.FC7RDFWOJSXTvVBO3tXllX6CPEf14n_UNST7w0bO2Dg",
     {
       cookies: {
         get(name: string) {
