e5356e1d3ff6a5a0aa83fc9d9b154565ee13ae14
SAMUEL NTI SARPONG <ce-sntisarpong8921@st.umat.edu.gh>
2025-09-18 06:47:24 +0000

    Try fixing this error: `Runtime Error: Error: useAuthContext must be use

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,11 +1,17 @@
+'use client';
+
+import AuthProvider from '@/context/auth-provider';
+
 export default function AuthLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-background p-4">
-      {children}
-    </main>
+    <AuthProvider>
+      <main className="flex min-h-screen flex-col items-center justify-center bg-background p-4">
+        {children}
+      </main>
+    </AuthProvider>
   );
 }
